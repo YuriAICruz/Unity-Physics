b34c22cc811bac4504e1496813ac1bf6adabbbed
Yuri Cruz <yuriaicruz@gmail.com>
2019-03-04 23:34:45 -0300

    init TwinSticks space physics

---
+++ Graphene/Physics/TwinStick/ShipStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Graphene.Physics.TwinStick
+{
+    [Serializable]
+    public class ShipStatus
+    {
+        public float Thrusters;
+        public float Shield;
+        public float Hull;
+        public float Weight;
+    }
+}
---
+++ Graphene/Physics/TwinStick/TwinStickShipPhysics.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Graphene.Physics.TwinStick
+{
+    public class TwinStickShipPhysics
+    {
+        public Rigidbody Rigidbody;
+        
+        private readonly ShipStatus _shipStatus;
+        private Vector3 _velocity;
+
+        public TwinStickShipPhysics(Rigidbody rigidbody, ShipStatus shipStatus)
+        {
+            Rigidbody = rigidbody;
+            
+            _shipStatus = shipStatus;
+        }
+
+        public void Move(Vector3 dir)
+        {
+            var pwd = _shipStatus.Thrusters / _shipStatus.Weight;
+            
+            dir = new Vector3(dir.x, 0, dir.y);
+
+            _velocity += dir * pwd;
+
+            _velocity = Vector3.ClampMagnitude(_velocity, _shipStatus.Thrusters*10);
+            
+            Rigidbody.velocity = _velocity;
+            
+            _velocity -= _velocity.normalized * pwd * 0.1f;
+            
+            Debug.Log($"_velocity: {_velocity}");
+
+        }
+
+        public void Look(Vector2 dir)
+        {
+            //Debug.Log($"Look: {dir}");
+        }
+    }
+}
